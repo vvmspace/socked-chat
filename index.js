b6619a87687570259761fb08ad0abc7aeee72b01
Vladimir V. Myagdeev <concertbil@gmail.com>
2017-10-03 19:44:16 +0500

    Hello world

---
+++ index.js
@@ -0,0 +1,8 @@
+var app = require('express')();
+var http = require('http').Server(app);
+app.get('/', function (req, res) {
+   res.send('<h1>Hello world</h1>');
+});
+http.listen(4000, function(){
+    console.log('listening on *:4000');
+});
